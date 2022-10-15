b0f24b2fdd543869f9fbc8f1574e493ad12eb7b3
Nikhil Deshmukh <nikhildeshmukh2210@gmail.com>
2022-10-15 16:14:18 +0530

    Rest client support added

---
+++ BaseService.js
@@ -0,0 +1,17 @@
+const userAction1 = async () => {
+    const response = await fetch('http://example.com/movies.json');
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+  }
+
+  const userAction2 = async () => {
+    const response = await fetch('http://example.com/movies.json', {
+      method: 'POST',
+      body: myBody, // string or object
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+  }
